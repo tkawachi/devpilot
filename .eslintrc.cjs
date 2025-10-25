38ae8cc1e652afe7828d008c7f9cf0f0b4908209
KAWACHI Takashi <tkawachi@gmail.com>
2025-10-25 10:21:18 +0900

    Setup CI lint typecheck test

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module"
+  },
+  env: {
+    node: true,
+    es2021: true
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  ignorePatterns: ["dist/**"],
+  rules: {}
+};
